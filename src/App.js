e63cb4be69f0cc4f87df5477c18814b2f5cd01c0
prem023 <phanishwarnath2@gmail.com>
2023-07-24 19:04:19 +0530

    developed current weather app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Outlet, createBrowserRouter } from "react-router-dom";
+import Home from "./components/Home";
+import Error from "./components/Error";
+import City from "./components/City";
+import WeatherReport from "./components/WeatherReport";
+import { useState } from "react";
+import LocationContext from "./components/LocationContext";
 
-function App() {
+const AppLayout = () => {
+  const [location, setLocation] = useState({
+    lat: 0,
+    lon: 0,
+  });
+
+  const [place, setPlace] = useState(["Kolkata", "IN"]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <LocationContext.Provider
+      value={{
+        location: location,
+        setLocation: setLocation,
+        place: place,
+        setPlace: setPlace,
+      }}
+    >
+      <Home />
+      <div className="w-1/5 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <Outlet />
+      </div>
+    </LocationContext.Provider>
   );
-}
+};
+
+const App = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <City />,
+      },
+      {
+        path: "/weather-report",
+        element: <WeatherReport />,
+      },
+    ],
+  },
+]);
 
 export default App;
